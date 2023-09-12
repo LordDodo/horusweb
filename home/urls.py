98ee73c30bb1e43c36543e108071810a26911da9
LordDodo <donovan.bailleul@icloud.com>
2023-09-12 13:55:50 +0200

    Added login page and member page redirection
    This update introduces a new login page to the web application. The login page includes a form for users to enter their username and password. Upon successful authentication, users are redirected to the member page.
    
    Key changes made:
    - Created the HTML template `login.html` for the login page.
    - Integrated the login form with CSRF token for protection against CSRF attacks.
    - Utilized Django's `messages` module to display error messages in case of authentication failure.
    - Configured the Django view `user_login` to handle the login form submission.
    - Implemented the `@login_required` decorator to secure access to the member page, ensuring only authenticated users can access it.

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.user_login, name="login"),  # Chemin pour la vue de connexion
+    path("profile/", views.profile, name="profile"),  # Chemin pour la vue de profil
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,25 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-def index(request):
-    return render(request, 'index.html')
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('profile')  # Rediriger vers la page de profil après la connexion
+        else:
+            messages.error(request, 'Identifiants incorrects. Veuillez réessayer.')  # Message d'erreur en cas d'échec d'authentification
+    return render(request, 'login.html')
+
+@login_required
+def profile(request):
+    user = request.user  # Récupérer l'utilisateur connecté
+    context = {
+        'username': user.username,
+        # Autres données que vous souhaitez passer au template...
+    }
+    return render(request, 'profile.html', context)
